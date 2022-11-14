f939b0dd133e2133841fd4a7f46490386cd0a460
pravinc3798 <pravin.choudhary88@gmail.com>
2022-11-14 13:35:49 +0530

    added addressbook entity

---
+++ RepositoryLayer/Entity/AddressBookEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class AddressBookEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ContacId { get; set; }
+        public string FullName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
